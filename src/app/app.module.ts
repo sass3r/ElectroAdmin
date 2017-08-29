00af6306dab424b71692b6d57c42632a334b1698
sasor <s.leamsi99@gmail.com>
2017-08-29 10:30:39 -0400

    [A] component header added with simple navbar template bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './common/header/header.component';
 
 export const environment = {
   production: false,
@@ -22,7 +23,8 @@ export const environment = {
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
