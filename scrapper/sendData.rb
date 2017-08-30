56c373060d83b379ff9b2a6c2c9c163f37e63472
Irvin cossio chavalier <irvin373@Irene.local>
2017-08-29 22:22:24 -0400

    scrapper

--- scrapper/sendData.rb
+++ scrapper/sendData.rb
@@ -1,5 +1,5 @@
 require 'creek'
-require 'rest-firebase'
+require 'firebase'
 
 class Matter
     def new(name, students)
@@ -65,6 +65,7 @@ end
 
 def getMatterName(sheet)
     name = sheet.rows.first['C3']
+    name = name.split('  ').last
     return name
 end
 
@@ -120,46 +121,9 @@ json = JSON.parse(matter.tojson)
 File.write('circuito.json', json)
 
 #firebase conection and publish
-# base_uri = 'https://electroinscripciones.firebaseio.com/'
-# firebase = Firebase::Client.new(base_uri)
-# response = firebase.push("materias", json)
+base_uri = 'https://electroinscripciones.firebaseio.com/'
+firebase = Firebase::Client.new(base_uri)
+response = firebase.push('laboratorios/', json)
+json = {key: response.body['name'] , name: matter.name}
+response = firebase.push('materias/', json)
 
-f = RestFirebase.new :site => 'https://electroinscripciones.firebaseio.com/',
-    :secret => 'secret',
-    :d => {:auth_data => 'something'},
-    :log_method => method(:puts),
-    # `timeout` in seconds
-    :timeout => 10,
-    # `max_retries` upon failures. Default is: `0`
-    :max_retries => 3,
-    # `retry_exceptions` for which exceptions should retry
-    # Default is: `[IOError, SystemCallError]`
-    :retry_exceptions =>
-    [IOError, SystemCallError, Timeout::Error],
-    # `error_callback` would get called each time there's
-    # an exception. Useful for monitoring and logging.
-    :error_callback => method(:p),
-    # `auth_ttl` describes when we should refresh the auth
-    # token. Set it to `false` to disable auto-refreshing.
-    # The default is 23 hours.
-    :auth_ttl => 82800,
-    # `auth` is the auth token from Firebase. Leave it alone
-    # to auto-generate. Set it to `false` to disable it.
-    :auth => false # Ignore auth for this example!
-
-# Streaming over 'users/tom'
-es = f.event_source('materias/')
-es.onopen   { |sock| p sock } # Called when connected
-es.onmessage{ |event, data, sock| p event, data } # Called for each message
-es.onerror  { |error, sock| p error } # Called whenever there's an error
-# Extra: If we return true in onreconnect callback, it would automatically
-#        reconnect the node for us if disconnected.
-es.onreconnect{ |error, sock| p error; @reconnect }
-
-# Start making the request
-es.start
-
-# Try to close the connection and see it reconnects automatically
-es.close
-
-p f.post('materias/', json)
