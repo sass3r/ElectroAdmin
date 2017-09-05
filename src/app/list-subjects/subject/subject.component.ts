66ed71acdf7c055bf45f74d4e1e6c9cb54d325bb
sasor <s.leamsi99@gmail.com>
2017-09-05 09:10:35 -0400

    methodo inscribir casi terminado, falta mostrar modal

--- src/app/list-subjects/subject/subject.component.ts
+++ src/app/list-subjects/subject/subject.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from '../../modal/modal.component';
 
 @Component({
     selector: 'app-subject',
@@ -8,17 +10,26 @@ import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable }
     styleUrls: ['./subject.component.sass']
 })
 export class SubjectComponent implements OnInit {
+
     inscribed: FirebaseListObservable<any>;
+    students$: FirebaseListObservable<any[]>;
     groups: FirebaseListObservable<any[]>;
     name: FirebaseObjectObservable<any>;
     limit: FirebaseObjectObservable<any>;
     key: String;
     limitInscribed: number = 0;
     overloadGroup: boolean = false;
-    constructor(private router: Router, private route: ActivatedRoute, private db: AngularFireDatabase) {
+
+    testcod = 201001274;
+
+    constructor(private router: Router,
+                private route: ActivatedRoute,
+                private db: AngularFireDatabase,
+                private modalService: NgbModal) {
         this.key = this.route.snapshot.paramMap.get('key');
         this.name = db.object(`/laboratorios/${this.key}/name`);
         this.groups = db.list(`/laboratorios/${this.key}/groups`);
+        this.students$ = db.list(`/laboratorios/${this.key}/students`);
         this.limit = db.object(`/laboratorios/${this.key}/limit`);
         this.limit.subscribe(
             (data)=>{
@@ -81,6 +92,29 @@ export class SubjectComponent implements OnInit {
     fakeSuscribe(){
         let fakeSys = this.getRandomInt();
         this.inscribed.push(fakeSys);
+    }
+
+    openModal(msj){
+        const modalRef = this.modalService.open(ModalComponent);
+        modalRef.componentInstance.message = msj;
+    }
+
+    inscribirme(){
+        this.students$.subscribe(
+            (students)=>{
+                if(students.length==0 || !students){
+                    console.log("no students data found");
+                    return false;
+                }
+                for(let student of students) {
+                    let codsys = student.codsys;
+                    if(codsys == this.testcod){
+                        this.inscribed.push(codsys);
+                        this.openModal('Congratulations!!!');
+                    }
+                }
+            }
+        );
     }
 
 }
--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-modal',
